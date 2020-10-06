a86dce1dc1f1cd359b17b76f3e2f9637078e9977
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2020-10-06 07:06:08 -0500

    Achieve full test coverage.

--- Calculator/CalculatorTests/Commands/CommandFactoryTest.cs
+++ Calculator/CalculatorTests/Commands/CommandFactoryTest.cs
@@ -1,12 +1,14 @@
 ﻿using CalculatorApp1;
 using CalculatorApp1.Commands;
 using NUnit.Framework;
+using System;
 
 namespace CalculatorTests.Commands
 {
     class CommandFactoryTest
     {
         protected CommandFactory GetTestSubject() => CommandFactory.Init(default);
+
         [Test]
         public void GetCommand_method_returns_an_AddCommand_when_given_an_Operator_Add_enum_value()
         {
@@ -21,6 +23,7 @@ namespace CalculatorTests.Commands
 
             Assert.That(instance, Is.InstanceOf<SubtractCommand>());
         }
+
         [Test]
         public void GetCommand_method_returns_a_DivideCommand_when_given_an_Operator_Divide_enum_value()
         {
@@ -28,12 +31,22 @@ namespace CalculatorTests.Commands
 
             Assert.That(instance, Is.InstanceOf<DivideCommand>());
         }
+
         [Test]
         public void GetCommand_method_returns_a_MultiplyCommand_when_given_an_Operator_Multiply_enum_value()
         {
             var instance = GetTestSubject().GetCommand(Operator.Multiply);
 
             Assert.That(instance, Is.InstanceOf<MultiplyCommand>());
+        }
+
+        [Test]
+        public void GetCommand_method_throws_an_argument_exception()
+        {
+            var invalid_operator_enum_value = unchecked((Operator)UInt64.MaxValue);
+            void shouldThrow() => GetTestSubject().GetCommand(invalid_operator_enum_value);
+
+            Assert.Throws<ArgumentException>(shouldThrow);
         }
     }
 }
